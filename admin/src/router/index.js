d219e019bde528d9294918f766aa6591acfc3a93
hmbool <1298541373@qq.com>
2020-01-31 17:56:49 +0800

    new

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 // import Home from '../views/Home.vue'
 import Main from '../views/Main.vue'
 import CategoryEdit from '../views/CategoryEdit.vue'
+import CategoryList from '../views/CategoryList.vue'
+
 
 Vue.use(VueRouter)
 
@@ -12,7 +14,11 @@ const routes = [
         name: 'main',
         component: Main,
         children: [
-            { path: '/categories/create', component: CategoryEdit }
+            { path: '/categories/create', component: CategoryEdit },
+            { path: '/categories/edit/:id', component: CategoryEdit, props: true },
+            { path: '/categories/list', component: CategoryList },
+
+            
         ]
     },
 ]
