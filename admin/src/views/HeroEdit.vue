b4d2381ee0c44fb51aa8b0bcd106693994cdf728
hmbool <1298541373@qq.com>
2020-02-02 20:27:01 +0800

    文章 富文本 广告位

--- admin/src/views/HeroEdit.vue
+++ admin/src/views/HeroEdit.vue
@@ -2,10 +2,13 @@
     <div class="about">
         <h1>{{id ? '编辑' : '新建'}}英雄</h1>
         <el-form label-width="120px" @submit.native.prevent="save">
-            <el-tabs :value="skills" type="border-card">
-                <el-tab-pane label="基本信息">
+            <el-tabs value="skills" type="border-card">
+                <el-tab-pane label="基础信息">
                     <el-form-item label="名称">
                         <el-input v-model="model.name"></el-input>
+                    </el-form-item>
+                    <el-form-item label="称号">
+                        <el-input v-model="model.title"></el-input>
                     </el-form-item>
                     <el-form-item label="头像">
                         <el-upload
@@ -14,19 +17,16 @@
                             :show-file-list="false"
                             :on-success="afterUpload"
                         >
-                            <img v-if="model.avater" :src="model.avater" class="avatar">
+                            <img v-if="model.avatar" :src="model.avatar" class="avatar">
                             <i v-else class="el-icon-plus avatar-uploader-icon"></i>
                         </el-upload>
-                    </el-form-item>
-                    <el-form-item label="称号">
-                        <el-input v-model="model.title"></el-input>
                     </el-form-item>
                     <el-form-item label="类型">
                         <el-select v-model="model.categories" multiple>
                             <el-option
                                 v-for="item of categories"
-                                :label="item.name"
                                 :key="item._id"
+                                :label="item.name"
                                 :value="item._id"
                             ></el-option>
                         </el-select>
@@ -63,12 +63,12 @@
                             v-model="model.scores.survive"
                         ></el-rate>
                     </el-form-item>
-                    <el-form-item label="顺丰出装">
+                    <el-form-item label="顺风出装">
                         <el-select v-model="model.items1" multiple>
                             <el-option
                                 v-for="item of items"
-                                :label="item.name"
                                 :key="item._id"
+                                :label="item.name"
                                 :value="item._id"
                             ></el-option>
                         </el-select>
@@ -77,8 +77,8 @@
                         <el-select v-model="model.items2" multiple>
                             <el-option
                                 v-for="item of items"
-                                :label="item.name"
                                 :key="item._id"
+                                :label="item.name"
                                 :value="item._id"
                             ></el-option>
                         </el-select>
@@ -89,16 +89,16 @@
                     <el-form-item label="对抗技巧">
                         <el-input type="textarea" v-model="model.battleTips"></el-input>
                     </el-form-item>
-                    <el-form-item label="团战技巧">
+                    <el-form-item label="团战思路">
                         <el-input type="textarea" v-model="model.teamTips"></el-input>
                     </el-form-item>
                 </el-tab-pane>
                 <el-tab-pane label="技能" name="skills">
-                    <el-button type="small" @click="model.skills.push({})">
-                        <i class="el-icon-plus">添加技能</i>
+                    <el-button size="small" @click="model.skills.push({})">
+                        <i class="el-icon-plus"></i>添加技能
                     </el-button>
                     <el-row type="flex" style="flex-wrap: wrap">
-                        <el-col :md="12" v-for="(item,index) in model.skills" :key="index">
+                        <el-col :md="12" v-for="(item, i) in model.skills" :key="i">
                             <el-form-item label="名称">
                                 <el-input v-model="item.name"></el-input>
                             </el-form-item>
@@ -107,17 +107,24 @@
                                     class="avatar-uploader"
                                     :action="$http.defaults.baseURL + '/upload'"
                                     :show-file-list="false"
-                                    :on-success="afterUpload"
+                                    :on-success="res => $set(item, 'icon', res.url)"
                                 >
                                     <img v-if="item.icon" :src="item.icon" class="avatar">
                                     <i v-else class="el-icon-plus avatar-uploader-icon"></i>
                                 </el-upload>
                             </el-form-item>
                             <el-form-item label="描述">
-                                <el-input type="textarea" v-model="item.description"></el-input>
+                                <el-input v-model="item.description" type="textarea"></el-input>
                             </el-form-item>
                             <el-form-item label="小提示">
-                                <el-input v-model="item.tips"></el-input>
+                                <el-input v-model="item.tips" type="textarea"></el-input>
+                            </el-form-item>
+                            <el-form-item>
+                                <el-button
+                                    size="small"
+                                    type="danger"
+                                    @click="model.skills.splice(i, 1)"
+                                >删除</el-button>
                             </el-form-item>
                         </el-col>
                     </el-row>
@@ -141,7 +148,7 @@ export default {
       items: [],
       model: {
         name: "",
-        avater: "",
+        avatar: "",
         scores: {
           difficult: 0
         }
@@ -150,13 +157,10 @@ export default {
   },
   methods: {
     afterUpload(res) {
-      // window.console.log(res)
-      // 显示赋值
-      // this.$set(this.model, 'avatar', res.url)
-      this.model.avater = res.url;
+      this.model.avatar = res.url;
     },
     async save() {
-      //   let res
+    //   let res;
       if (this.id) {
         await this.$http.put(`rest/heroes/${this.id}`, this.model);
       } else {
@@ -171,7 +175,6 @@ export default {
     async fetch() {
       const res = await this.$http.get(`rest/heroes/${this.id}`);
       this.model = Object.assign({}, this.model, res.data);
-      //   this.model = res.data;
     },
     async fetchCategories() {
       const res = await this.$http.get(`rest/categories`);
@@ -183,36 +186,10 @@ export default {
     }
   },
   created() {
-    this.fetchCategories();
     this.fetchItems();
+    this.fetchCategories();
     this.id && this.fetch();
   }
 };
 </script>
 
-
-<style>
-.avatar-uploader .el-upload {
-  border: 1px dashed #d9d9d9;
-  border-radius: 6px;
-  cursor: pointer;
-  position: relative;
-  overflow: hidden;
-}
-.avatar-uploader .el-upload:hover {
-  border-color: #409eff;
-}
-.avatar-uploader-icon {
-  font-size: 28px;
-  color: #8c939d;
-  width: 178px;
-  height: 178px;
-  line-height: 178px;
-  text-align: center;
-}
-.avatar {
-  width: 178px;
-  height: 178px;
-  display: block;
-}
-</style>
